472988dd6fb6e7aa4a4da16ea3585426cdc11df4
Mara Oliveira <maradelynie@gmail.com>
2023-01-14 20:43:50 -0300

    InputText style fixed for new input types
    it adds dark type an larger sizes.
    Makes the component more reusable.

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import './style.css';
 
@@ -6,20 +6,30 @@ type inputTextProps = {
   label: string;
   id: number | string;
   size: 'sm' | 'md' | 'lg';
+  style?: 'dark';
+  icon?: ReactNode;
 };
 
-export default function InputText({ label, id, size }: inputTextProps) {
+export default function InputText({
+  label,
+  id,
+  size,
+  style,
+  icon
+}: inputTextProps) {
+  const inputStyle = (icon ? 'icon ' : '') + size + ' ' + style;
   return (
-    <div className="inputText_container">
+    <div className={'inputText_container ' + size}>
+      {icon ? <div className="inputText_icon">{icon}</div> : <></>}
       <label htmlFor={id + '_' + label}>{label}</label>
       <input
-        className={'inputText_' + size}
+        className={'inputText ' + inputStyle}
         type="text"
         name={id + '_' + label}
         id={id + '_' + label}
         data-testid={id + '_' + label}
         placeholder={label}
-        maxLength={6}
+        maxLength={size === 'sm' ? 6 : undefined}
       />
     </div>
   );
